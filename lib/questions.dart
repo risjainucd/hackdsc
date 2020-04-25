95b8baa95959e0e2e22a01279ccf70f04e964339
hritvik <hritvik.agarwal@gmail.com>
2020-04-26 04:18:41 +0530

    First Commit

---
+++ lib/questions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red[900],
+        title: new Text('Please answer the following questions')
+      ),
+    );
+  }
+}
